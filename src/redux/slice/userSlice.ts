6acd71a31869da71f5652ec527bb63e49fe6c4df
Omotayo Eyimofe <eyimofeomotayo@gmail.com>
2024-05-18 21:52:31 +0100

    set new initial state

--- src/redux/slice/userSlice.ts
+++ src/redux/slice/userSlice.ts
@@ -6,7 +6,9 @@ const initialState = {
   },
   user: {
     email: '',
-    isAuthenticated: true,
+    isAuthenticated: false,
+    role: '',
+    token: '',
   },
 };
 
@@ -16,6 +18,18 @@ const userSlice = createSlice({
   reducers: {
     setIsFetching: (state) => {
       state.state.isFetching = true;
+    },
+    loginUser: (state, action) => {
+      state.user.email = action.payload.email;
+      state.user.isAuthenticated = true;
+      state.user.role = action.payload.role;
+      state.user.token = action.payload.token;
+    },
+    logoutUser: (state) => {
+      state.user.email = '';
+      state.user.isAuthenticated = false;
+      state.user.role = '';
+      state.user.token = '';
     },
   },
 });
